65b79925258cc408579a125e076e679f6926e8e0
FU Wenhui <ffffwh@icloud.com>
2016-07-19 10:44:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Utility for 'ThinkPad Compact Bluetooth Keyboard with TrackPoint' (0B47189) for OSX
+
+- Adapted from https://github.com/lentinj/tp-compact-keyboard.
+- It
+  - switches on fn-lock
+  - changes middle button mode
+
+It needs to be run with `sudo`.
---
+++ tpkb/tp.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdint>
+
+#include "hidapi.h"
+
+int main()
+{
+	const uint16_t vendor_id = 0x17EF;
+	const uint16_t product_id = 0x6048;
+
+    if (hid_init()) {
+        return -1;
+    }
+
+	hid_device *dev = hid_open(vendor_id, product_id, 0);
+    
+	if (dev == 0) {
+		printf("TP Keyboard not found.\n");
+		return 0;
+	}
+
+	unsigned char buf[4];
+
+	// Fn lock ON
+	buf[0] = 0x18;
+	buf[1] = 0x05;
+	buf[2] = 0x01;
+	hid_write(dev, buf, 3);
+
+	// Middle button
+	buf[0] = 0x18;
+	buf[1] = 0x09;
+	buf[2] = 0x00;
+	hid_write(dev, buf, 3);
+	
+	hid_close(dev);
+	return 0;
+}
