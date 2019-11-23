23fe3da5f8af3a295638a86cd01cfebff76f2bda
Fu Wenhui <_@_._>
2019-11-23 20:10:56 +0800

    Toggle fn-lock for Logitech K480

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-Utility for 'ThinkPad Compact Bluetooth Keyboard with TrackPoint' (0B47189) for OSX
+Utility for 'Logitech K480 Keyboard' for macOS.
 
-- Adapted from https://github.com/lentinj/tp-compact-keyboard.
-- It
-  - switches on fn-lock
-  - changes middle button mode
+Adapted from https://github.com/FSpark/k480_conf/blob/master/k480_conf.c
+
+It switches on fn-lock.
 
 It needs to be run with `sudo`.
+
+This keyboard has several variations. Please tweak `product_id` and `k480_seq_fkeys_on` and then `make`.
--- tpkb/tp.cpp
+++ tpkb/tp.cpp
@@ -1,38 +1,43 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdint>
+#include <sys/ioctl.h>
 
 #include "hidapi.h"
 
 int main()
 {
-    const uint16_t vendor_id = 0x17EF;
-    const uint16_t product_id = 0x6048;
+    int r;
+    const uint16_t vendor_id = 0x046d;
+
+    // TWEAK ME
+    // const uint16_t product_id = 0xb330;
+    // const uint16_t product_id = 0xb33c;
+    const uint16_t product_id = 0xb33d;
     
-    if (hid_init()) {
+    if (hid_init() == -1) {
+        printf("failed at hid_init()\n");
         return -1;
     }
     
     hid_device *dev = hid_open(vendor_id, product_id, 0);
+
     
     if (dev == 0) {
-        printf("TP Keyboard not found. Have you run it with 'sudo'?\n");
+        printf("K480 Keyboard not found. Have you run it with 'sudo'?\n");
         return 0;
     }
-    
-    unsigned char buf[4];
-    
-    // Fn lock ON
-    buf[0] = 0x18;
-    buf[1] = 0x05;
-    buf[2] = 0x01;
-    hid_write(dev, buf, 3);
-    
-    // Middle button
-    buf[0] = 0x18;
-    buf[1] = 0x09;
-    buf[2] = 0x00;
-    hid_write(dev, buf, 3);
+
+    // TWEAK ME
+    //const char k480_seq_fkeys_on[]  = {0x10, 0xff, 0x08, 0x1c, 0x00, 0x00, 0x00};
+    const unsigned char k480_seq_fkeys_on[]  = {0x10, 0xff, 0x0b, 0x1c, 0x00, 0x00, 0x00};
+    // code from https://github.com/FSpark/k480_conf/blob/master/k480_conf.c
+
+    r = hid_write(dev, k480_seq_fkeys_on, sizeof(k480_seq_fkeys_on));
+    if (r == -1) {
+        printf("failed at hid_write() for fn-lock\n");
+        return -1;
+    }
     
     hid_close(dev);
     return 0;
